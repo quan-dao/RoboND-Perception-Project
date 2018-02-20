0b87cb49742cf391a9a496c24ceb8410f8e3494c
Quan Dao <daominhquan9909@gmail.com>
2018-02-19 21:29:50 -0800

    add object regconition

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -123,21 +123,38 @@ def pcl_callback(pcl_msg):
     cluster_pub.publish(ros_cluster_clould)
 
 # Exercise-3 TODOs:
+	# Create empty lists to receive object lables and object point cloud
+    detected_objects = []
+    detected_objects_labels = []
+    for index, pts_list in enumerate(cluster_indices):
+		# Grab the points for the cluster from the extracted outliers (ransac_objects)
+        pcl_cluster = ransac_objects.extract(pts_list)
+        # Convert pcl to ROS
+        ros_cluster = pcl_to_ros(pcl_cluster)
+        # Extract histogram feature
+        chists = compute_color_histograms(ros_cluster, using_hsv=True)
+        normals = get_normals(ros_cluster)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
+        # Make the prediction, retrieve the label for the cluster and add this label to detected_objects_labels list
+        prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
+        label = encoder.inverse_transform(prediction)[0]
+        detected_objects_labels.append(label)
+    	# Publish a label into RViz
+    	label_pos = list(white_cloud[pts_list[0]])
+    	label_pos[2] += .4
+    	object_markers_pub.publish(make_label(label,label_pos, index))
+    	# Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
 
-    # Classify the clusters! (loop through each detected cluster one at a time)
-
-        # Grab the points for the cluster
-
-        # Compute the associated feature vector
-
-        # Make the prediction
-
-        # Publish a label into RViz
-
-        # Add the detected object to the list of detected objects.
-
+    # Print out the number and names of detected object
+    rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
     # Publish the list of detected objects
-
+    detected_objects_pub.publish(detected_objects)
+   
     # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
     # Could add some logic to determine whether or not your object detections are robust
     # before calling pr2_mover()
@@ -196,8 +213,15 @@ if __name__ == '__main__':
     # TODO: Create Publishers
     ransac_pub = rospy.Publisher("/ransac_segmentation", pc2.PointCloud2, queue_size=1)
     cluster_pub = rospy.Publisher("/extracted_cluster", pc2.PointCloud2, queue_size=1)
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
     # TODO: Load Model From disk
+    model = pickle.load(open('model.sav', 'rb'))
+    clf = model['classifier']
+    encoder = LabelEncoder()
+    encoder.classes_ = model['classes']
+    scaler = model['scaler']
 
     # Initialize color_list
     get_color_list.color_list = []
